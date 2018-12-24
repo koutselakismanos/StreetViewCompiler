f67c4d6047c12d2caeecded6af270bc9a41b02b7
koutselakismanos <koutselakismanos@gmail.com>
2018-12-24 14:48:48 +0200

    trying to get routes + gitignore

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 const express = require('express');
+const morgan = require('morgan');
+const request = require('request');
 
 const app = express();
 const port = 5000;
 
-
+app.use(morgan('dev'));
 
 app.get('/api/', (req, res) =>
 {
-	res.sendFile('views/test.html', { root: __dirname });
+	res.status(200).sendFile('views/test.html', { root: __dirname });
+});
+
+app.get('/api/route/', (req, response) =>
+{
+	let positions = {
+		routes: []
+	};
+	request({
+		url: "https://maps.googleapis.com/maps/api/directions/json?origin=Chicago,IL&destination=Los+Angeles,CA&waypoints=Joplin,MO|Oklahoma+City,OK&key=AIzaSyDM1Md63YaQY-nPkpoK60q8S8MJ_2pjFgc",
+		json: true
+	}, (err, res, json) =>
+		{
+			if (err) throw err;
+			json.routes[0].legs.filter(leg => Object.getOwnPropertyNames(leg).includes("steps"))
+				.forEach(leg => leg['steps'].forEach(step =>
+				{
+					// console.log(step.start_location);
+					positions.routes.push(step.start_location);
+					positions.routes.push(step.end_location);
+				}));
+
+			response.json(positions);
+			// console.log(positions);
+			// 	.forEach(element =>
+			// {
+			// 	console.log(element.start_location);
+			// 	positions.push(element.start_location);
+			// });
+		})
+
+	// console.log(positions);
 });
 
 app.listen(port, () => { console.log(`Server started on port ${port}`); });
