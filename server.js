7cdca23a7f526f38b4f4a8823ff1e40b47a7e6f1
koutselakismanos <koutselakismanos@gmail.com>
2019-01-07 04:34:34 +0200

    made livestream of the video

--- server.js
+++ server.js
@@ -168,9 +168,44 @@ function compilee()
 
 app.get('/api/video', (req, res) =>
 {
-    res.writeHead(200, { 'Content-type': 'video/mp4' })
-    let rs = fs.createReadStream("assets/demo/meme.mp4");
-    rs.pipe(res);
+    // res.writeHead(200, { 'Content-type': 'video/mp4' })
+    // let rs = fs.createReadStream("assets/demo/meme.mp4");
+    // rs.on('error', (err) => { console.log(err) })
+    // rs.pipe(res);
+    const path = 'assets/demo/pepe.mp4';
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    console.log(stat, fileSize, range)
+
+    if (range)
+    {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1
+        const chunksize = (end - start) + 1
+        const file = fs.createReadStream(path, { start, end });
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+
+        res.writeHead(206, head);
+        file.pipe(res);
+    }
+    else
+    {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+    }
 });
 
 app.post('/api/log', (req, response) =>
